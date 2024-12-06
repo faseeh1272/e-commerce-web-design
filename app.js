d0b6e88007d4ec63fbc715d69b75e486d686c537
faseeh1272 <syedfaseehnaqvi2@gmail.com>
2024-12-06 11:43:16 -0800

    profile add

---
+++ app.js
@@ -0,0 +1,25 @@
+function showAlert(){
+    alert("item has been added to the cart!")
+}
+
+function  showAlert1(){
+    alert("second item has been added to the cart!")
+}
+function  showAlert2(){
+    alert("third item has been added to the cart!")
+}
+function  showAlert3(){
+    alert("four item has been added to the cart!")
+}
+function  showAlert4(){
+    alert("five item has been added to the cart!")
+}
+function  showAlert5(){
+    alert("six item has been added to the cart!")
+}
+function  showAlert6(){
+    alert("seven item has been added to the cart!")
+}
+function  showAlert7(){
+    alert("eight item has been added to the cart!")
+}
